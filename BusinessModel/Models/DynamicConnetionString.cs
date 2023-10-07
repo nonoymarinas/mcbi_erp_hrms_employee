2f1e29067f3f8d57d975ed22d5dab87f54ed92bd
FELICIANO JR MARINAS <nonoymarinas@live.com>
2023-10-07 15:33:44 +0800

    commit

--- BusinessModel/Models/DynamicConnetionString.cs
+++ BusinessModel/Models/DynamicConnetionString.cs
@@ -8,8 +8,8 @@ namespace BusinessModel
 {
     public static class GlobalValues
     {
-        //public static string? ConnectionString { get; set; } = "Server=tcp:speedxserver.database.windows.net,1433;Initial Catalog=ERP_SPEEDX_DB;Persist Security Info=False;User ID=nonoymarinas;Password=Nonoy10@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        public static string? ConnectionString { get; set; } = "Server=tcp:speedxserver.database.windows.net,1433;Initial Catalog=ERP_SPEEDX_DB_Development;Persist Security Info=False;User ID=nonoymarinas;Password=Nonoy10@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public static string? ConnectionString { get; set; } = "Server=tcp:speedxserver.database.windows.net,1433;Initial Catalog=ERP_SPEEDX_DB;Persist Security Info=False;User ID=nonoymarinas;Password=Nonoy10@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        //public static string? ConnectionString { get; set; } = "Server=tcp:speedxserver.database.windows.net,1433;Initial Catalog=ERP_SPEEDX_DB_Development;Persist Security Info=False;User ID=nonoymarinas;Password=Nonoy10@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
     }
 }
